8036188dae6094e960a69148cf057b088e1dd6f2
himaloyash <himaloyashis98@gmail.com>
2023-05-02 12:50:06 +0600

    footer done

---
+++ src/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <div className=''>
+            <div className="navbar bg-amber-200">
+                <div className="flex-1">
+                    <h2 className="btn btn-ghost normal-case text-xl font-extrabold"><i>Toothsome Space</i></h2>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                        <li>
+                            <Link>Home</Link>
+                            <Link>Blog</Link>
+                            <Link>Login</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ src/Page/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h2>Toothsome Space is a popular Restaurant</h2>
+        </div>
+    );
+};
+
+export default Footer;
