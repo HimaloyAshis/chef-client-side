f2b632d71017d57929ae943071cf162777db5921
himaloyash <himaloyashis98@gmail.com>
2023-05-04 22:16:40 +0600

    rating

--- src/Page/Home/ChefExplainer.jsx
+++ src/Page/Home/ChefExplainer.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { FaRegThumbsUp, FaThumbsUp } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
+
 const ChefExplainer = ({chef}) => {
 
     const [like, setLike] = useState(false)
@@ -10,13 +11,14 @@ const ChefExplainer = ({chef}) => {
 
     const likeButton = ()=>{
         setLike(true)
+    
     }
 
     return (
         <div  className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 
             <img className="rounded-t-lg object-cover flex-grow" src={chef.chefPicture} alt="" />
-
+            
             <div className="p-5">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Name: {chef.chefName}
                 </h5>
--- src/Page/RecipesDetails/ThreeRecipes.jsx
+++ src/Page/RecipesDetails/ThreeRecipes.jsx
@@ -1,26 +1,52 @@
+import { Rating } from '@smastrom/react-rating';
 import React, { useState } from 'react';
-import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import { FaCheckCircle, FaCircle, FaHeart, FaRegHeart, FaRegStar, FaStar } from 'react-icons/fa';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// import ReactStars from "react-rating-stars-component";
 
-const ThreeRecipes = ({recipe}) => {
+
+const ThreeRecipes = ({ recipe }) => {
     const [favour, setFavour] = useState(false)
-
-
+    const [rating, setRating] = useState(0)
+   
     const favorite = () => {
         setFavour(true)
+        toast('Your favorite item added')
+
+    }
+
+    const changeIt = () =>{
+
     }
 
     return (
         <div key={recipe.name} className="card w-96 mt-10 m-10 bg-base-100 shadow-xl">
             <div className=" card-body">
+                <ToastContainer></ToastContainer>
                 <h2 className="card-title text-xl font-bold">{recipe.recipeName}</h2>
                 <h2 className='text-xl font-bold'>ingredients</h2>
                 {
-                    recipe.ingredients.map(ingrd => <p> {ingrd}</p>)
+                    recipe.ingredients.map(ingrd => <p><FaCircle className='text-gray-500 inline-flex'></FaCircle> {ingrd}</p>)
                 }
                 <p><span className='text-xl'>Cooking method :</span> {recipe.cookingMethod}</p>
                 <p><span className='text-xl'>Rating :</span>{recipe.rating}</p>
+                <div  className='h-8 w-8'>
+
+                    {/* <ReactStars activeColor={'yellow'} size={20} isHalf={true} onChange={changeIt}></ReactStars> */}
+                    {/* <Rating
+
+                        className=''
+                        placeholderRating={recipe?.rating}
+                        readOnly
+                        emptySymbol={<FaRegStar ></FaRegStar>}
+                        placeholderSymbol={<FaStar className='text-yellow-400'></FaStar>}
+                        fullSymbol={<FaStar></FaStar>}
+                    /> */}
+                    {/* <Rating style={{ maxWidth: 250 }} value={rating} onChange={setRating} /> */}
+                </div>
                 <div className="card-actions justify-end">
-                    <button onClick={favorite}>{favour ? <FaHeart  className='h-8 w-8   text-red-500'></FaHeart> : <FaRegHeart className='h-8 w-8 text-red-500'></FaRegHeart>}</button>
+                    <button onClick={favorite} disabled={favour}>{favour ? <FaHeart className='h-8 w-8   text-red-500'></FaHeart> : <FaRegHeart className='h-8 w-8 text-red-500'></FaRegHeart>}</button>
                 </div>
             </div>
         </div>
