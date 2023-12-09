2cc9640cb276e2d66e33df5d1266739e321c88c6
himaloyash <himaloyashis98@gmail.com>
2023-12-09 12:36:44 +0600

    color changed for buton

--- src/Shared/Footer/NavBar.jsx
+++ src/Shared/Footer/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { AuthContext } from '../../Provider/AuthProvider';
 import { XMarkIcon, Bars3Icon } from '@heroicons/react/24/solid'
@@ -6,24 +6,29 @@ import { XMarkIcon, Bars3Icon } from '@heroicons/react/24/solid'
 
 const NavBar = () => {
 
-    const {user, logOut} = useContext(AuthContext)
+    const [Open, setOpen] = useState(false)
+    const { user, logOut } = useContext(AuthContext)
 
-    const outToLog= ()=>{
+    const outToLog = () => {
         logOut()
-        .then()
-        .catch(error=>console.log(error.message))
-        
+            .then()
+            .catch(error => console.log(error.message))
+
     }
 
     return (
         <div className=''>
 
             <div>
-                <span>
-                    
+                <span onClick={() => setOpen(!Open)} className='md:hidden'>
+
+                    {
+                        Open ? <XMarkIcon className='h-6 w-6 text-blue-500 cursor-pointer' /> :
+                            <Bars3Icon className='h-6 w-6 text-blue-500 cursor-pointer' />
+                    }
                 </span>
-            </div>            
-            <div className="navbar bg-[#4e706c]">
+            </div>
+            <div className={`navbar bg-[#4e706c] `}>
 
                 <div className="flex-1">
                     <h2 className="  default text-2xl font-extrabold"><i>Toothsome Space</i></h2>
@@ -31,22 +36,22 @@ const NavBar = () => {
                 <div className="">
                     <ul className="flex gap-2 px-1">
                         <li>
-                            <NavLink to={`/`} className={({isActive})=>isActive? 'active' : 'default'} >
+                            <NavLink to={`/`} className={({ isActive }) => isActive ? 'active' : 'default'} >
                                 Home
-                            </NavLink>                           
+                            </NavLink>
                         </li>
                         <li>
-                            <NavLink to={`/blog`} className={({isActive})=>isActive? 'active' : 'default'}>
+                            <NavLink to={`/blog`} className={({ isActive }) => isActive ? 'active' : 'default'}>
                                 Blog
                             </NavLink>
                         </li>
                         <li className='inline-flex gap-2'>
-                            {user? <><img className='rounded-full h-8 w-8' src={user?.photoURL} title={user?.displayName}  alt="Author" /> <button className='default' onClick={outToLog}>LogOut</button></> : <NavLink to="/login" className={({isActive})=>isActive? 'active' : 'default'}>
+                            {user ? <><img className='rounded-full h-8 w-8' src={user?.photoURL} title={user?.displayName} alt="Author" /> <button className='default' onClick={outToLog}>LogOut</button></> : <NavLink to="/login" className={({ isActive }) => isActive ? 'active' : 'default'}>
                                 Login
                             </NavLink>}
                         </li>
                         <li>
-                            
+
                         </li>
                     </ul>
                 </div>
